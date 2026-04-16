25a9bdd4c1d4d14a0a2927ee2232f0bc8130b1c0
rosendo <rosendc30@gmail.com>
2026-04-15 23:14:39 -0300

    cards continuando

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div className="container">
       <header className="flex flex-col items-center justify-center text-center gap-6 px-4">
 
-        <h1 className=" text-4xl font-medium text-purple-950">10,000+ of our users love our products.</h1>
+        <h1 className=" text-4xl font-medium text-purple-950 p-4">10,000+ of our users love our products.</h1>
 
         <p className="text-dark-grayish-magenta font-light leading-relaxed">
           We only provide great products combined with excellent customer service.
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,19 @@
 import Smith from '../assets/image-colton.jpg';
 export function Card() {
     return (
-        <div className="flex flex-col items-center justify-start w-full h-120 text-white bg-[var(--VeryDarkMagenta)]/90 rounded-sm p-8">
-            <div className='flex items-center gap-8'>
-                <img className='rounded-full w-14 h-14' src={Smith} alt="" />
-                <div>
-                    <h3 className="text-lg font-bold mb-2">Colton Smith</h3>
-                    <p className="text-[var(--SoftPink)] opacity-50 text-lg">Verified Buyer</p>
+        <div className="flex flex-col items-center justify-start w-full  text-white bg-[var(--VeryDarkMagenta)]/90 rounded-sm mb-4 p-4 ">
+            <div className='flex items-center justify-start gap-4 py-6'>
+                <img className='rounded-full w-12 h-12' src={Smith} alt="photo user" />
+                <div className='flex flex-col items-start justify-start'>
+                    <h3 className="text-lg font-bold text-[var(--LightGrayishMagenta)]">Colton Smith</h3>
+                    <p className="text-[var(--SoftPink)]/40 text-lg">Verified Buyer</p>
                 </div>
             </div>
-            <div>
-                <p>
-                    " We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent! "
-                </p>
-            </div>
+
+            <p className='px-2 font-semibold text-[var(--LightGrayishMagenta)]'>
+                " We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent! "
+            </p>
+
         </div>
     );
 }
